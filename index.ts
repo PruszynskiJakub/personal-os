6781c3c1b68cfb0a3e98c44e5c8f9ab149c42b61
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2025-12-31 20:54:46 +0700

    Use the prompt and llm service to do dummy request

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import {Hono} from "hono";
 import {logger} from "hono/logger";
 import {prettyJSON} from 'hono/pretty-json';
-import type {Message} from "./src/types/Message";
+import type {CoreMessage} from "ai";
+import {completion} from "./src/services/llm.service.ts";
+import {prompt as thinkPrompt} from "./src/prompts/agent.think.ts"
 
 const app = new Hono();
 
@@ -9,10 +11,19 @@ app.use('*', logger());
 app.use('*', prettyJSON());
 
 app.post("/chat", async (c) => {
-    const body = await c.req.json<{ messages: Message[] }>();
+    const body = await c.req.json<{ messages: CoreMessage[] }>();
     const lastUserMessage = body.messages.findLast((m) => m.role === "user");
 
-    return c.json({response: lastUserMessage});
+    const thinkingResult = await completion.text({
+        messages: [
+            {role: "system", content: thinkPrompt()},
+            ...body.messages
+        ],
+        temperature: 0,
+        maxTokens: 4000
+    })
+
+    return c.json({response: thinkingResult});
 });
 
 
