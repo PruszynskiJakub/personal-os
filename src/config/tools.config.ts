e085b5d7eceffd5a40c36ca2208c9a20e3fc1fc3
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2026-01-02 21:27:48 +0700

    Create tool registry

---
+++ src/config/tools.config.ts
@@ -0,0 +1,32 @@
+export const toolRegistry = [
+    {
+        name: "logbook",
+        description: "Use whenever users wants to register, update or remove diving or climbing records.",
+        actions: [
+            {
+                name: "add_dive",
+                description: "Use this action whenever user wants to record one or more dives",
+                instructions: `
+                    {
+                        "dives" : [
+                            {
+                                spot: string, //required
+                                max_depth: number, //required
+                                duration: number, //required
+                                salty: boolean, //optional
+                                weights: number, //optional
+                                tank: number, //optional
+                                tank_type: "alu" | "steel", //optional
+                                start_air: number,
+                                end_air: number,
+                                date: string | date, //required,
+                                dive_school: string,
+                                score : number // optional
+                            } 
+                        ]
+                    }
+                `
+            }
+        ]
+    }
+]
