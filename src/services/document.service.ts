b0b85999394801c576e9a925c429bb78aec51142
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2026-01-03 17:59:19 +0700

    Refactor documentService

--- src/services/document.service.ts
+++ src/services/document.service.ts
@@ -1,25 +1,31 @@
 import {v4 as uuidv4} from 'uuid';
 import type {Document} from "../types/agent.ts";
 
-const documentService = {
 
-    createDocument: async (params: {
-        uuid?: string,
-        conversation_uuid: string,
-        text: string,
-    }): Promise<Document> => {
-        const document_uuid = params.uuid ?? uuidv4();
-        const now = new Date().toISOString()
+function createDocumentService() {
 
-        return {
-            uuid: document_uuid,
-            conversation_uuid: params.conversation_uuid,
-            text: params.text,
-            created_at: now,
-            updated_at: now,
+    return {
+
+        async createDocument(params: {
+            uuid?: string,
+            conversation_uuid: string,
+            text: string,
+        }): Promise<Document> {
+            const document_uuid = params.uuid ?? uuidv4();
+            const now = new Date().toISOString()
+
+            return {
+                uuid: document_uuid,
+                conversation_uuid: params.conversation_uuid,
+                text: params.text,
+                created_at: now,
+                updated_at: now,
+            }
         }
     }
 }
+
+const documentService = createDocumentService();
 
 export {
     documentService,
