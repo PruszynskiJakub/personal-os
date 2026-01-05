191daaaef28971354d45d2f1a82963035df79dd5
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2026-01-05 14:18:13 +0700

    Filter out system messages

--- src/middlewares/files.middleware.ts
+++ src/middlewares/files.middleware.ts
@@ -1,5 +1,37 @@
 import type {Context, Next} from "hono";
+import {z} from "zod";
+
+const BaseMessageContent = z.union([
+    z.string(),
+    z.array(
+        z.object({
+            type: z.string(),
+            text: z.string().optional(),
+            data: z.string().optional(),
+            image: z.string().optional(),
+        })
+    )
+]);
+
+const RequestSchema = z.object({
+    messages: z.array(
+        z.object({
+            role: z.enum(['system', 'user', 'assistant', 'tool']),
+            content: BaseMessageContent
+        })
+    ),
+    stream: z.boolean().optional(),
+})
 
 export const filesMiddleware = async (c: Context, next: Next) => {
+    const request = c.get('request') || {}
+    const parsed_request = RequestSchema.parse(request)
+    const non_system_messages = parsed_request.messages.filter(m => m.role !== 'system')
+
+    c.set('request', {
+        ...parsed_request,
+        messages: non_system_messages
+    });
+
     return await next();
 }
